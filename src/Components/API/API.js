aecb4e229edf6e2d40c204006566f6fad8457257
Noah <bitterhippo@gmail.com>
2022-03-03 23:35:44 +0900

    final

--- src/Components/API/API.js
+++ src/Components/API/API.js
@@ -6,7 +6,9 @@ const API = {
       redirect: 'follow'
     }
 
-    fetch('https://swapi.dev/api/people/1/', requestOptions)
+    const query = Math.floor(Math.random() * (83 - 1) + 1);
+
+    fetch(`https://swapi.dev/api/people/${query}/`, requestOptions)
       .then(response => response.text())
       .then(result => state(JSON.parse(result)))
       .catch(err => console.log('Error', err))
